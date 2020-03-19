a8f649a19cb8219e7b0cc644376909d12db25426
Devian Kapp <forza9882@gmail.com>
2020-03-19 19:22:41 +0200

    initial commit.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var countDownDate = new Date("April 29, 2020 00:00:00").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (2000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (2000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (2000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (2000 * 60)) / 1000);
+
+  document.getElementById("LaunchDate").innerHTML =
+    days + "d  " + hours + "h  " + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("LaunchDate").innerHTML = distance;
+  }
+}, 1000);
