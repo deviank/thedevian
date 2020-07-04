3fa4d821c02fbac581aa3a00250e07f48ae23888
Devian Kapp <forza9882@gmail.com>
2020-07-04 10:12:44 +0200

    Adding code for countdown ticker.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-var countDownDate = new Date("April 29, 2020 00:00:00").getTime();
-var x = setInterval(function() {
+var countDownDate = new Date("July 29, 2020 00:00:00").getTime();
+
+var x = setInterval(function () {
   var now = new Date().getTime();
   var distance = countDownDate - now;
-  var days = Math.floor(distance / (2000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (2000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (2000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (2000 * 60)) / 1000);
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   document.getElementById("LaunchDate").innerHTML =
     days + "d  " + hours + "h  " + minutes + "m " + seconds + "s ";
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("LaunchDate").innerHTML = distance;
+    document.getElementById("LaunchDate").innerHTML = "days";
   }
 }, 1000);
